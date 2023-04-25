2ea81b6ecb2403abe2e1585f45c5ffb7527d41a9
hyoudouzixi <wang.13099@buckeyemail.osu.edu>
2023-04-24 21:16:15 -0400

    update on background

--- js/eventListeners.js
+++ js/eventListeners.js
@@ -1,50 +1,29 @@
 window.addEventListener('keydown', (event) => {
-  if (player.preventInput) return
-  switch (event.key) {
-    case 'w':
-      for (let i = 0; i < doors.length; i++) {
-        const door = doors[i]
-
-        if (
-          player.hitbox.position.x + player.hitbox.width <=
-            door.position.x + door.width &&
-          player.hitbox.position.x >= door.position.x &&
-          player.hitbox.position.y + player.hitbox.height >= door.position.y &&
-          player.hitbox.position.y <= door.position.y + door.height
-        ) {
-          player.velocity.x = 0
-          player.velocity.y = 0
-          player.preventInput = true
-          player.switchSprite('enterDoor')
-          door.play()
-          return
-        }
-      }
-      if (player.velocity.y === 0) player.velocity.y = -25
-
-      break
-    case 'a':
-      // move player to the left
-      keys.a.pressed = true
-      break
-    case 'd':
-      // move player to the right
-      keys.d.pressed = true
-      break
-  }
+    switch (event.key) {
+        // if key w is pressed, move up
+        case 'w':
+            if (player.velocity.y === 0) {
+                player.velocity.y = -20;
+            }
+            break
+        // if key a is pressed, returns true
+        case 'a':
+            keys.a.pressed = true;
+            break
+        // if key d is pressed, returns true
+        case 'd':
+            keys.d.pressed = true;
+            break
+    }
 })
 
 window.addEventListener('keyup', (event) => {
-  switch (event.key) {
-    case 'a':
-      // move player to the left
-      keys.a.pressed = false
-
-      break
-    case 'd':
-      // move player to the right
-      keys.d.pressed = false
-
-      break
-  }
+    switch (event.key) {
+        case 'a':
+            keys.a.pressed = false;
+            break;
+        case 'd':
+            keys.d.pressed = false;
+            break;
+    }
 })
